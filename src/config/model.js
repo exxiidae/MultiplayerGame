de606316d8eef82551b2b2d0baa3daf9264c8ce8
exxiidae <selevuv@mail.ru>
2026-06-19 02:11:49 +0300

    123

--- src/config/model.js
+++ src/config/model.js
@@ -1,6 +1,6 @@
 ﻿export const MODELS_CONFIG = {
     url : [
-        '../../models/hahahahah.glb',   // 0 — Scout
+        '../../models/scout.glb',   // 0 — Scout
         '../../models/assault.glb',     // 1 — Assault
         '../../models/lazership.glb',  // 2
         '../../models/asteroid2.glb', // 3
